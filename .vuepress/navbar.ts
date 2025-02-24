9941248497a6c6c808bd8d3921b99cff9275ef8a
coderJCK <kjc15102375622@163.com>
2025-02-24 18:19:20 +0800

    2025-0224-2

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -2,10 +2,11 @@ import {NavItem} from "vuepress/config";
 
 export default [
     {
-        text: "常用",
+        text: "其他",
         items: [
-            {text: "常用网址",link: "/常用/常用网址.md"},
-            {text: "常用命令",link: "/常用/常用命令.md"},
+            {text: "常用网址",link: "/其他/常用网址.md"},
+            {text: "常用命令",link: "/其他/常用命令.md"},
+            {text: "常用快捷键",link: "/其他/常用快捷键.md"},
         ]
     },
     {
